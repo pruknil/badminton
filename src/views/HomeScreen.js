d8274d46255495e20935b56534c505f3193dc755
pruknil <pruknil@pruks-air>
2018-11-04 07:22:01 +0700

    merge tab

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,16 +1,113 @@
 import React from 'react';
-import {
-  AsyncStorage,
-  Button,
-  StyleSheet,
-  View,
-} from 'react-native';
+import Expo, { AppLoading, Asset, Font } from 'expo';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { View, Image, Dimensions } from 'react-native';
+import { DrawerNavigator, DrawerItems } from 'react-navigation';
 
+import Components from '../drawer/components';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const CustomDrawerContentComponent = props => (
+  <View style={{ flex: 1, backgroundColor: '#43484d' }}>
+    <View style={{ marginTop: 40, justifyContent: 'center', alignItems: 'center' }}>
+      <Image
+        source={require('../images/logo.png')}
+        style={{ width: SCREEN_WIDTH * 0.57 }}
+        resizeMode="contain"
+      />
+    </View>
+    <View style={{ marginLeft: 10 }}>
+      <DrawerItems {...props} />
+    </View>
+  </View>
+);
+
+
+const MainRoot = DrawerNavigator(
+  {
+
+    Components: {
+      path: '/components',
+      screen: Components,
+    },
+
+  },
+  {
+    initialRouteName: 'Components',
+    contentOptions: {
+      activeTintColor: '#548ff7',
+      activeBackgroundColor: 'transparent',
+      inactiveTintColor: '#ffffff',
+      inactiveBackgroundColor: 'transparent',
+      labelStyle: {
+        fontSize: 15,
+        marginLeft: 0,
+      },
+    },
+    drawerWidth: SCREEN_WIDTH * 0.8,
+    contentComponent: CustomDrawerContentComponent,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle',
+  }
+);
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Welcome to the app!',
+     state = {
+    isReady: false,
+  };
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+     /* require('./assets/images/bg_screen1.jpg'),
+      require('./assets/images/bg_screen2.jpg'),
+      require('./assets/images/bg_screen3.jpg'),
+      require('./assets/images/bg_screen4.jpg'),
+      require('./assets/images/user-cool.png'),
+      require('./assets/images/user-hp.png'),
+      require('./assets/images/user-student.png'),
+      require('./assets/images/avatar1.jpg'),*/
+    ]);
+
+    const fontAssets = cacheFonts([FontAwesome.font, Ionicons.font]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          // onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <MainRoot />
+    );
+  }
+
+  static navigationOptions = {
+      header: null
     };
-  
+    /*
     render() {
       return (
         <View style={styles.container}>
@@ -27,12 +124,5 @@ export default class HomeScreen extends React.Component {
     _signOutAsync = async () => {
       await AsyncStorage.clear();
       this.props.navigation.navigate('Auth');
-    };
+    };*/
   }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
